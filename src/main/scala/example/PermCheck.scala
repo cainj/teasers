1885b13d295acc39c3823eabc222566d08629112
Jay Cain <jcain@placeable.com>
2017-07-21 14:28:10 -0600

    codility PermCheck

---
+++ src/main/scala/example/PermCheck.scala
@@ -0,0 +1,70 @@
+package example
+
+/**
+ * https://codility.com/programmers/lessons/4-counting_elements/perm_check/
+ *
+ * A non-empty zero-indexed array A consisting of N integers is given.
+ *
+ * A permutation is a sequence containing each element from 1 to N once, and only once.
+ *
+ * For example, array A such that:
+ *
+ * A[0] = 4
+ * A[1] = 1
+ * A[2] = 3
+ * A[3] = 2
+ * is a permutation, but array A such that:
+ *
+ * A[0] = 4
+ * A[1] = 1
+ * A[2] = 3
+ * is not a permutation, because value 2 is missing.
+ *
+ * The goal is to check whether array A is a permutation.
+ *
+ * Write a function:
+ *
+ * object Solution { def solution(a: Array[Int]): Int }
+ * that, given a zero-indexed array A, returns 1 if array A is a permutation and 0 if it is not.
+ *
+ * For example, given array A such that:
+ *
+ * A[0] = 4
+ * A[1] = 1
+ * A[2] = 3
+ * A[3] = 2
+ * the function should return 1.
+ *
+ * Given array A such that:
+ *
+ * A[0] = 4
+ * A[1] = 1
+ * A[2] = 3
+ * the function should return 0.
+ *
+ * Assume that:
+ *
+ * N is an integer within the range [1..100,000];
+ * each element of array A is an integer within the range [1..1,000,000,000].
+ * Complexity:
+ *
+ * expected worst-case time complexity is O(N);
+ * expected worst-case space complexity is O(N), beyond input storage (not counting the storage required for input arguments).
+ * Elements of input arrays can be modified.
+ */
+object PermCheck {
+
+  def solution(a: Array[Int]): Int = {
+    if (a.length == 1 && a(0) != 1) 0
+    else {
+      val x = a.sortWith(_ < _)
+      var i = 0
+      var start = 1
+      while (start == x(i) && i < x.length - 1) {
+        start += 1
+        i += 1
+      }
+      if (start == x(i)) 1 else 0
+    }
+  }
+}
---
+++ src/test/scala/example/PermCheckSpec.scala
@@ -0,0 +1,19 @@
+package example
+
+import org.scalatest.{ Matchers, FlatSpec }
+
+/**
+ *
+ */
+class PermCheckSpec extends FlatSpec with Matchers {
+  "The PermCheck object" should "find words" in {
+    PermCheck.solution(Array(4, 3, 1, 2)) shouldEqual 1
+    PermCheck.solution(Array(4, 3, 1)) shouldEqual 0
+    PermCheck.solution(Array(4, 3, 2)) shouldEqual 0
+    PermCheck.solution(Array(3, 2, 1)) shouldEqual 1
+    PermCheck.solution(Array(5, 3, 2, 1)) shouldEqual 0
+    PermCheck.solution(Array(2)) shouldEqual 0
+    PermCheck.solution(Array(1)) shouldEqual 1
+    PermCheck.solution(Array(0)) shouldEqual 0
+  }
+}
