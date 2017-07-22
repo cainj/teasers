8955541ff3424401218627c3fb8a1bcc7e1d159c
Jay Cain <jcain@placeable.com>
2017-07-21 21:59:27 -0600

    MaxCounter by Codility

---
+++ src/main/scala/example/MaxCounters.scala
@@ -0,0 +1,23 @@
+package example
+
+/**
+ *
+ */
+object MaxCounters {
+
+  def solution(n: Int, a: Array[Int]): Array[Int] = {
+    val maxCounter = n + 1
+    val rover = new Array[Int](maxCounter)
+    var max = 0
+    for (i <- 0 until a.length) {
+      if (a(i) == maxCounter && max != 0)
+        for (j <- rover.indices) rover(j) = max
+      else if (a(i) != maxCounter) {
+        val current = rover(a(i)) + 1
+        rover(a(i)) = current
+        max = Math.max(max, current)
+      }
+    }
+    rover.tail
+  }
+}
---
+++ src/test/scala/example/MaxCountersSpec.scala
@@ -0,0 +1,15 @@
+package example
+
+import org.scalatest.{ Matchers, FlatSpec }
+
+/**
+ *
+ */
+class MaxCountersSpec extends FlatSpec with Matchers {
+  "The MaxCounter object" should "find words" in {
+    MaxCounters.solution(5, Array(3, 4, 4, 6, 1, 4, 4)) shouldEqual Array(3, 2, 2, 4, 2)
+    MaxCounters.solution(4, Array(5, 5, 5, 5, 5, 5, 5)) shouldEqual Array(0, 0, 0, 0)
+    MaxCounters.solution(5, Array(6, 6, 6, 6, 6, 6, 6)) shouldEqual Array(0, 0, 0, 0, 0)
+  }
+}
+
