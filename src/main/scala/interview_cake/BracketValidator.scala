b8a97ceca916d8d0ca4d43bd18f20b24ff716d2b
Jay Cain <cain.jay@gmail.com>
2017-08-03 21:00:07 -0600

    Interview cake bracket validators: https://www.interviewcake.com/question/java/bracket-validator

--- src/main/scala/interview_cake/BracketValidator.scala
+++ src/main/scala/interview_cake/BracketValidator.scala
@@ -13,7 +13,7 @@ import scala.util.{ Failure, Success, Try }
  * Write an efficient function that tells us whether or not an input string's openers
  * and closers are properly nested.
  */
-object BrackeValidator {
+object BracketValidator {
 
   def validate(n: String): Boolean = {
     var s = n
