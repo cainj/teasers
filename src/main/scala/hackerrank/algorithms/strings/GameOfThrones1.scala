344cd3b9a0f0efb4f78cdaa80834df75db561cd7
Jay Cain <cain.jay@gmail.com>
2017-08-06 15:10:27 -0600

    HackerRank: Game of Thrones - I

--- src/main/scala/hackerrank/algorithms/strings/GameOfThrones1.scala
+++ src/main/scala/hackerrank/algorithms/strings/GameOfThrones1.scala
@@ -10,41 +10,12 @@ object GameOfThrones1 {
   def main(args: Array[String]) {
     val sc = new java.util.Scanner(System.in);
     val s = sc.next();
-
-    s.toList.permutations.map { _.mkString } find {
-      letters => letters.reverse == letters
-    } map { _ => println("YES") } getOrElse println("NO")
-  }
-
-  /**
-   * Without the scala built in version
-   * @param chars
-   * @return
-   */
-  def permutation(chars: List[Char]): List[List[Char]] = chars match {
-    case List(_) => List(chars)
-    case _ =>
-      for {
-        c <- chars
-        (l, r) = chars span { c!= }
-        ys <- permutation(l ++ r.tail)
-      } yield c :: ys
-  }
-
-  /**
-    * Not in place O(n) in time O(n +1) in space
-    *
-    * Without the scala built in version
-    * @param chars
-    * @return
-    */
-  def reverse(chars: List[Char]): String = {
-
-    @tailrec
-    def doReverse(chars: List[Char], acc: List[Char]): List[Char] = chars match {
-      case Nil => acc
-      case head :: tail => doReverse(tail, head :: acc)
+    val m = s.foldLeft(Map.empty[Char, Int]) {
+      (rover, c) =>
+        val x = rover.getOrElse(c, 0)
+        rover + (c -> (x + 1))
     }
-    doReverse(chars, Nil) mkString
+    val odds = m.count(_._2 % 2 == 1)
+    if (odds > 2) println("NO") else println("YES")
   }
 }
