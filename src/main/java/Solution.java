442533c28fdb7ea08a5a4d6a398cfaf348157c4f
Jay Cain <cain.jay@gmail.com>
2017-08-20 17:18:08 -0600

    Iterative inversion

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,9 +1,30 @@
-
+import java.util.Arrays;
 
 class Solution {
+    public static void main(String[] args) {
+    int t = 5;
+    System.out.println(t);
 
-    public static void main(String args[])
-    {
+    for (int i = 0, n = 46; i < t;
+    i++, n += (int)Math.ceil(Math.log(n * n))) {
 
+        int k = n - ((int)Math.ceil(Math.sqrt(n)));
+        System.out.println(n + " " + k);
+
+        int a[] = new int[n];
+        Arrays.fill(a, 0, n, 0);
+
+        if (i % 2 == 0) {
+            Arrays.fill(a, 0, 2, -15);
+            Arrays.fill(a, 5, n, 10);
+        }
+        else {
+            Arrays.fill(a, 0, k >> 1, -15);
+            Arrays.fill(a, k + 1, n, 10);
+        }
+
+        for (int ai : a) System.out.print(ai + " ");
+        System.out.println();
     }
+}
 }
