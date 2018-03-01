6cd97c08f942d6e4d5c92b0b1869c4ee12835ecb
James Cain <cain.jay@gmail.com>
2018-03-01 15:08:35 -0700

    https://www.hackerrank.com/challenges/pairs/problem

--- src/main/java/hackerrank/algorithims/graphtheory/RoadLibraries.java
+++ src/main/java/hackerrank/algorithims/graphtheory/RoadLibraries.java
@@ -1,3 +1,5 @@
+package hackerrank.algorithims.graphtheory;
+
 import java.util.LinkedList;
 import java.util.Scanner;
 
---
+++ src/main/scala/hackerrank/algorithms/search/Pairs.scala
@@ -0,0 +1,32 @@
+package hackerrank.algorithms.search
+
+import scala.collection.mutable
+
+/**
+  * https://www.hackerrank.com/challenges/pairs/problem
+  */
+object Pairs {
+
+  def pairs(k: Int, arr: Array[Int]): Int = {
+    var sum = mutable.HashMap.empty[Int, Int]
+    var diff = mutable.HashMap.empty[Int, Int]
+    arr foreach { num =>
+      if (num - k > 0) diff += (num - k -> num)
+      sum += (num -> (num + k))
+    }
+    diff.toList.intersect(sum.toList).length
+  }
+
+  def main(args: Array[String]) {
+    val sc = new java.util.Scanner(System.in)
+    val n = sc.nextInt()
+    val k = sc.nextInt()
+    val arr = new Array[Int](n)
+    for (arr_i <- 0 until n) {
+      arr(arr_i) = sc.nextInt()
+    }
+    val result = pairs(k, arr)
+    println(result)
+  }
+}
+
