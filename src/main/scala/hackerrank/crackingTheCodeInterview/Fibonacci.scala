2bb5ca0beef7d2e49c25e59a91439cd45d686227
James Cain <cain.jay@gmail.com>
2018-03-05 12:51:29 -0700

    Using streams for memoization.

--- src/main/scala/hackerrank/crackingTheCodeInterview/Fibonacci.scala
+++ src/main/scala/hackerrank/crackingTheCodeInterview/Fibonacci.scala
@@ -1,17 +1,14 @@
 package hackerrank.crackingTheCodeInterview
 
 /**
-  * https://www.hackerrank.com/challenges/ctci-fibonacci-numbers/problem
-  */
+ * https://www.hackerrank.com/challenges/ctci-fibonacci-numbers/problem
+ */
 object Fibonacci {
 
-  def fibonacci(x: Int): Int = {
-    if (x == 0) 0
-    else if (x == 1) 1
-    else fibonacci(x - 1) + fibonacci(x - 2)
-  }
+  lazy val fib: Stream[Int] = 0 #:: 1 #:: fib.zip(fib.tail).map(p => p._1 + p._2)
+
   def main(args: Array[String]) {
     /** This will handle the input and output**/
-    println(fibonacci(readInt()))
+    println(fib take (readInt() + 1) last)
   }
 }
